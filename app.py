4fbd0d336f52399f9497f6ffea9bf33365ada175
Sanjeevvisuu <139469409+Sanjeevvisuu@users.noreply.github.com>
2025-02-02 11:50:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World! This is a static Flask app.'
+    return 'Hello, World! This is a static Flask app.Tested webhook'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
